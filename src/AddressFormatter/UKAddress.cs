033615c8a25a58a28cd8b046a4838902fd1db787
Chris Jones <cmj.chris.jones@googlemail.com>
2020-04-22 02:18:04 +0100

    Updated the namespaces

--- src/AddressFormatter/UKAddress.cs
+++ src/AddressFormatter/UKAddress.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Text;
 
-namespace AddressFormatter
+namespace CJTech.AddressFormatter
 {
     public class UKAddress
     {
